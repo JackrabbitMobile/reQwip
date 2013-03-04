f137f32a53d210bfca5c4f0cf1165a697ae3ba25
Jonathan Rosenberg <jon@jackrabbitmobile.com>
2013-03-03 19:00:45 -0800

    update listing model

--- js/app/models/listingModel.js
+++ js/app/models/listingModel.js
@@ -10,46 +10,8 @@ define(["jquery", "underscore", "backbone",  'backbone_forms'], function($, _, B
         listing_picture: { type: 'List', listType: 'Text' },
         description : "Text",
         condition : "Number"
-	},
-	defaults: function() {
-		return {
-			//'date' : new Date(),
-			
-			'email' : "your_email@here.com",
-			'name' : 'Firstname Lastname Here',
-			'profile_picture': "/img/3dslogo.png",
-			'participant_role': "role"
-		}
 	}
 });
+
 return ListingModel;
-/*
- var listing = new Listing({
- id: "1.json"
- // name:"Dirty Harry",
- // email:"bro@freeemail.net",
- // company:"3 Day Startup",
- // portfolio:"thebest.com"
- });
-
- listing.fetch({
- error: function(model, response) {
- console.log("error response");
- console.log(response);
- },
- success: function(model, response){
- console.log("sucess response");
- console.log(response);
- console.log("fetch name "+listing.get('email'));
- console.dir(listing.toJSON());
- }
- });
-
- console.log("1st name "+listing.get('email'));
- console.dir(listing.toJSON());
- //listing.set({company:"Jackrabbit Mobile"});
- listing.fetch();
- console.log("2nd name "+listing.get('email'));
- console.dir(listing.toJSON());
- */
 });
