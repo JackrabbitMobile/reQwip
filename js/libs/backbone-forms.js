81881c11ba0439c25ecba3e70b824a2838ecefe2
Sri Sonti <sri.sonti@gmail.com>
2013-03-04 07:18:58 -0600

    Started making front-end changes to have design match the PDFs.

--- js/libs/backbone-forms.js
+++ js/libs/backbone-forms.js
@@ -2229,76 +2229,81 @@ Form.editors = (function() {
   //DEFAULT TEMPLATES
   Form.setTemplates({
     
-    //HTML
-    form: '\
-      <form class="bbf-form">{{fieldsets}}</form>\
-    ',
-    
-    fieldset: '\
-      <fieldset>\
-        <legend>{{legend}}</legend>\
-        <ul>{{fields}}</ul>\
-      </fieldset>\
-    ',
-    
-    field: '\
-      <li class="bbf-field field-{{key}}">\
-        <label for="{{id}}">{{title}}</label>\
-        <div class="bbf-editor">{{editor}}</div>\
-        <div class="bbf-help">{{help}}</div>\
-        <div class="bbf-error">{{error}}</div>\
-      </li>\
-    ',
+      //HTML
+      form: '\
+        <form class="form-horizontal">{{fieldsets}}</form>\
+      ',
 
-    nestedField: '\
-      <li class="bbf-field bbf-nested-field field-{{key}}" title="{{title}}">\
-        <label for="{{id}}">{{title}}</label>\
-        <div class="bbf-editor">{{editor}}</div>\
-        <div class="bbf-help">{{help}}</div>\
-        <div class="bbf-error">{{error}}</div>\
-      </li>\
-    ',
+      fieldset: '\
+        <fieldset>\
+          <legend>{{legend}}</legend>\
+          {{fields}}\
+        </fieldset>\
+      ',
 
-    list: '\
-      <div class="bbf-list">\
-        <ul>{{items}}</ul>\
-        <div class="bbf-actions"><button type="button" data-action="add">Add</div>\
-      </div>\
-    ',
+      field: '\
+        <div class="control-group field-{{key}}">\
+          <label class="control-label" for="{{id}}">{{title}}</label>\
+          <div class="controls">\
+            {{editor}}\
+            <div class="help-inline">{{error}}</div>\
+            <div class="help-block">{{help}}</div>\
+          </div>\
+        </div>\
+      ',
 
-    listItem: '\
-      <li>\
-        <button type="button" data-action="remove" class="bbf-remove">&times;</button>\
-        <div class="bbf-editor-container">{{editor}}</div>\
-      </li>\
-    ',
+      nestedField: '\
+        <div class="field-{{key}}">\
+          <div title="{{title}}" class="input-xlarge">{{editor}}\
+            <div class="help-inline">{{error}}</div>\
+          </div>\
+          <div class="help-block">{{help}}</div>\
+        </div>\
+      ',
 
-    date: '\
-      <div class="bbf-date">\
-        <select data-type="date" class="bbf-date">{{dates}}</select>\
-        <select data-type="month" class="bbf-month">{{months}}</select>\
-        <select data-type="year" class="bbf-year">{{years}}</select>\
-      </div>\
-    ',
+      list: '\
+        <div class="bbf-list">\
+          <ul class="unstyled clearfix">{{items}}</ul>\
+          <button class="btn bbf-add" data-action="add">Add</button>\
+        </div>\
+      ',
 
-    dateTime: '\
-      <div class="bbf-datetime">\
-        <div class="bbf-date-container">{{date}}</div>\
-        <select data-type="hour">{{hours}}</select>\
-        :\
-        <select data-type="min">{{mins}}</select>\
-      </div>\
-    ',
+      listItem: '\
+        <li class="clearfix">\
+          <div class="pull-left">{{editor}}</div>\
+          <button type="button" class="btn bbf-del" data-action="remove">&times;</button>\
+        </li>\
+      ',
 
-    'list.Modal': '\
-      <div class="bbf-list-modal">\
-        {{summary}}\
-      </div>\
-    '
-  }, {
+      date: '\
+        <div class="bbf-date">\
+          <select data-type="date" class="bbf-date">{{dates}}</select>\
+          <select data-type="month" class="bbf-month">{{months}}</select>\
+          <select data-type="year" class="bbf-year">{{years}}</select>\
+        </div>\
+      ',
 
-    //CLASSNAMES
-    error: 'bbf-error'
+      dateTime: '\
+        <div class="bbf-datetime">\
+          <p>{{date}}</p>\
+          <p>\
+            <select data-type="hour" style="width: 4em">{{hours}}</select>\
+            :\
+            <select data-type="min" style="width: 4em">{{mins}}</select>\
+          </p>\
+        </div>\
+      ',
+
+      'list.Modal': '\
+        <div class="bbf-list-modal">\
+          {{summary}}\
+        </div>\
+      '
+    }, {
+  
+      //CLASSNAMES
+      error: 'error' //Set on the field tag when validation fails
+
 
   });
 
